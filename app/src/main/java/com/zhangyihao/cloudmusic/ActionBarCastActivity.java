df97ab6ff44f52bfcd2135c68f64d15decab41b9
zhangyihao <zhangyihao@jiuqi.com.cn>
2016-03-03 20:32:52 +0800

    add LogHelper class

---
+++ app/src/main/java/com/zhangyihao/cloudmusic/ActionBarCastActivity.java
@@ -0,0 +1,9 @@
+package com.zhangyihao.cloudmusic;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhangyihao on 2016-3-3.
+ */
+public abstract class ActionBarCastActivity extends AppCompatActivity {
+}
