e015c820d81a6b6665ace489d39082c52afabd6d
zhangyihao <zyh5540@163.com>
2016-03-06 23:24:15 +0800

    just commit something

--- app/src/main/java/com/zhangyihao/cloudmusic/ActionBarCastActivity.java
+++ app/src/main/java/com/zhangyihao/cloudmusic/ActionBarCastActivity.java
@@ -1,8 +1,13 @@
 package com.zhangyihao.cloudmusic;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.google.android.libraries.cast.companionlibrary.cast.VideoCastManager;
+import com.google.android.libraries.cast.companionlibrary.cast.callbacks.VideoCastConsumerImpl;
 import com.zhangyihao.cloudmusic.util.LogHelper;
 
 /**
@@ -12,5 +17,19 @@ public abstract class ActionBarCastActivity extends AppCompatActivity {
     private final static String TAG = LogHelper.makeLogTag(ActionBarCastActivity.class);
 
     private static final int DELAY_MILLIS = 1000;
+
     private VideoCastManager mVideoCastManager;
+    private MenuItem mMediaRouteMenuItem;
+    private Toolbar mToolbar;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
+    private boolean mToolbarInitalized;
+
+    private int mItemToOpenWhenDrawerCloses = -1;
+
+    private final VideoCastConsumerImpl mCastConsumer = new VideoCastConsumerImpl() {
+
+    };
+
 }
