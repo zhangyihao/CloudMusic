965ff86e873d24adab42ad5076c15be2acc06728
zhangyihao <zhangyihao@jiuqi.com.cn>
2016-03-07 16:44:18 +0800

    add PrefUtil class

--- app/src/main/java/com/zhangyihao/cloudmusic/ActionBarCastActivity.java
+++ app/src/main/java/com/zhangyihao/cloudmusic/ActionBarCastActivity.java
@@ -1,14 +1,22 @@
 package com.zhangyihao.cloudmusic;
 
+import android.app.MediaRouteButton;
+import android.os.Handler;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.media.MediaRouter;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.libraries.cast.companionlibrary.cast.VideoCastManager;
 import com.google.android.libraries.cast.companionlibrary.cast.callbacks.VideoCastConsumerImpl;
+import com.github.amlcurran.showcaseview.ShowcaseView;
+import com.github.amlcurran.showcaseview.targets.ViewTarget;
 import com.zhangyihao.cloudmusic.util.LogHelper;
+import com.zhangyihao.cloudmusic.util.PrefUtils;
 
 /**
  * Created by zhangyihao on 2016-3-3.
@@ -30,19 +38,53 @@ public abstract class ActionBarCastActivity extends AppCompatActivity {
 
     private final VideoCastConsumerImpl mCastConsumer = new VideoCastConsumerImpl() {
         @Override
-        public void onConnected() {
-            super.onConnected();
-        }
-
-        @Override
-        public void onDisconnected() {
-            super.onDisconnected();
+        public void onFailed(int resourceId, int statusCode) {
+            LogHelper.d(TAG, "onFailed ", resourceId, " status ", statusCode);
         }
 
         @Override
         public void onConnectionSuspended(int cause) {
-            super.onConnectionSuspended(cause);
+            LogHelper.d(TAG, "onConnectionSuspended() was called with cause: ", cause);
+        }
+
+        @Override
+        public void onConnectivityRecovered() {
+        }
+
+        @Override
+        public void onCastDeviceDetected(final MediaRouter.RouteInfo info) {
+            // FTU stands for First Time Use:
+            if (!PrefUtils.isFtuShown(ActionBarCastActivity.this)) {
+                // If user is seeing the cast button for the first time, we will
+                // show an overlay that explains what that button means.
+                PrefUtils.setFtuShown(ActionBarCastActivity.this, true);
+
+                LogHelper.d(TAG, "Route is visible: ", info);
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        if (mMediaRouteMenuItem.isVisible()) {
+                            LogHelper.d(TAG, "Cast Icon is visible: ", info.getName());
+                            showFtu();
+                        }
+                    }
+                }, DELAY_MILLIS);
+            }
         }
     };
+
+    private void showFtu() {
+        Menu menu = mToolbar.getMenu();
+        View view = menu.findItem(R.id.media_route_menu_item).getActionView();
+        if(view !=null && view instanceof MediaRouteButton) {
+            new ShowcaseView.Builder(this)
+                    .setTarget(new ViewTarget(view))
+                    .setContentTitle("Touch to cast")
+                    .hideOnTouchOutside()
+                    .build();
+
+        }
+    }
 
 }
---
+++ app/src/main/java/com/zhangyihao/cloudmusic/util/PrefUtils.java
@@ -0,0 +1,25 @@
+package com.zhangyihao.cloudmusic.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Util for seeting and accessing {@link android.content.SharedPreferences} for the current Application
+ * Created by zhangyihao on 2016-3-7.
+ */
+public class PrefUtils {
+    private static final String PREF_NAMESPACE = "com.zhangyihao.cloudmusic.util.PREFS";
+    private static final String FTU_SHOWN = "ftu_shown";// stands for first to use  是否第一次使用
+
+    public static SharedPreferences getPreferences(Context context) {
+        return context.getSharedPreferences(PREF_NAMESPACE, Context.MODE_PRIVATE);
+    }
+
+    public static void setFtuShown(Context context, boolean shown) {
+        getPreferences(context).edit().putBoolean(FTU_SHOWN, shown).apply();
+    }
+
+    public static boolean isFtuShown(Context context) {
+        return getPreferences(context).getBoolean(FTU_SHOWN, false);
+    }
+}
